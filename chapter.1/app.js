5569f94d6097f8e97d8ef981135e5e8dd6bb8c48
Mirza Suban Baig <subanbaig3@gmail.com>
2024-02-22 18:01:43 +0500

    Assignemnt Has been Completed

---
+++ chapter.1/app.js
@@ -0,0 +1,81 @@
+alert("Chapter-1")
+// Q-1
+// 1.First Name 
+alert("Mirza Suban")
+// 2.Last Name 
+alert("Baig")
+// 3.Email 
+alert("subanbaig3@gmail.com")
+// 4.Phone N0. 
+alert("03118448348")
+// 5.Password 
+alert("suban789456")
+// Q-2 
+alert("You're learning Java Script!")
+// Q-3
+alert("Welcome to my Website. Hope you all like the products")
+// Q-4
+document.write("Error! Please enter a Valid Password")
+// Q-5
+document.write(" \n Welcome to JS Land \n Happy Coding!")
+// Q-6
+console.log("Hello...I can run JS through my web browser's console")
+// Q-7
+var x= "Welocme to JS Land"
+var y= "Happy Coding"
+console.log(x + "\n" + y)
+
+alert("Chapter-2")
+// Q-1
+var x = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(x)
+// Q-2
+var x =("Yah! I can write HTML content through JavaScript")
+document.write(x)
+// Q-3
+var x=("I am trying to learn from the Book")
+var y=("A Smarter \n Way to Learn JavaScript")
+alert( x+" "+ y )
+// Q-4
+var email=("subanbaig3@gmail")
+var x= email
+alert(x)
+// Q-5
+var myName=("Mirza Suban Baig")
+var fullName= myName
+alert(fullName)
+// Q-6
+var message="Hello World"
+alert(message)
+// Q-7
+var Sname=("Jhone Doe \n")
+var age=("15 years old \n")
+var job=("Web Developer")
+alert (Sname + age + job)
+
+alert("Chapter-3")
+// Q-1
+var age =("I am 22 years old")
+alert(age)
+// Q-2
+var number = 10
+var finalresult= ("You have visited this site" +" "+ number +" "+"times")
+alert(finalresult)
+// Q-3
+var birthyear=("My birth year is 2001")
+document.write(birthyear)
+// Q-4
+var Visitorname=("Jhone Doe")
+var Producttitle=("T-Shirts")
+var Quantity=("5")
+document.write( "\n" + Visitorname +"  "+"Ordered"+" "+ Quantity+" "+ Producttitle+" "+"from XYZ Clothing Store")
+// Q-5
+var caseQty = x+ y
+var x=44
+var y=100
+alert(x+y)
+// Q-6
+var merchTotal = 100
+var shippingCharge = 10
+var orderTotal = merchTotal + shippingCharge
+alert(orderTotal)
